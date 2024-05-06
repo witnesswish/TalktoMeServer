06c305aea39ccfd468564f39f3930762437f5577
peter <387902891@qq.com>
2024-05-06 02:24:38 +0000

    main update

--- main.cpp
+++ main.cpp
@@ -7,28 +7,21 @@
 #include <stdlib.h>
 #include <openssl/crypto.h>
 
-void tagmain(int argc, char *argv[])
-{
-    for(int i=0; i<argc; ++i)
-    {
-        std::cout<< argv[i] <<std::endl;
-    }
-}
 
 /** isport
 return
 0 for is legel port, bettwen 100-5656
 -1 for not a legel port
 **/
-int isport(char *argv1)
+/*int isport(char *argv1)
 {
     return 0;
-}
+}*/
 
 int main(int argc, char *argv[])
 {
     std::regex reg_ipv4("(((2(5[0-5]|[0-4])\\d)|([0-1]?\\d{1,2}))\\.){3}(2(5[0-5]|[0-4])\\d|[0-1]?\\d{1,2})$");
-    std::regex reg_port("\\d{3,4}");
+    std::regex reg_port("\\d{4,5}");
     if(argc == 1)
     {
         Server serv;
@@ -40,27 +33,28 @@ int main(int argc, char *argv[])
         std::vector<std::string> options;
         for(int i=1; i<argc; ++i)
         {
-            options.push_back(argv[i]);
-            std::string sig_option = options[i].substr(0, 1);
+            std::string arg = std::string(argv[i]);
+            std::string sig_option = arg.substr(0, 1);
             if(sig_option == "-")
             {
-                sig_option = options[i].substr(1,2);
+                sig_option = arg.substr(1,2);
                 if(sig_option == "-")
                 {
-                    if(strncasecmp(argv[i], "host", strlen("host")) == 0)
+                    sig_option = arg.substr(2,arg.size());
+                    if(sig_option == "host")
                     {
                         Server serv(argv[i], 3434);
                         serv.StartServer();
                         return 0;
                     }
-                    if(strncasecmp(argv[i], "port", strlen("port")) == 0)
+                    if(sig_option == "port")
                     {
                         int port = atoi(argv[i]);
                         Server serv("127.0.0.1", port);
                         serv.StartServer();
                         return 0;
                     }
-                    if(strncasecmp(argv[i], "help", strlen("help")) == 0)
+                    if(sig_option == "help")
                     {
                         std::cout<< "Usage: \n -h --host for host, basically is 127.0.0.1. \n -p --port for port, basically is 3434." <<std::endl;
                         return 0;
@@ -78,7 +72,7 @@ int main(int argc, char *argv[])
         {
             if(std::regex_match(argv[1], reg_port))
             {
-                tagmain(argc, argv);
+                tag(argc, argv);
                 return 0;
             }
             else
@@ -90,7 +84,7 @@ int main(int argc, char *argv[])
         }
         if(std::regex_match(argv[1], reg_ipv4))
         {
-            tagmain(argc, argv);
+            tag(argc, argv);
             return 0;
         }
         else
